27514f2d268ee0713121e4d80611fbbaf97dc6d3
Jonathan De Leon <jrdeleon@usc.edu>
2021-04-10 15:10:22 -0700

    Add webapp project

---
+++ webapp/components/NavbarLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-shrink-0 flex items-center">
+    <img
+      class="block lg:hidden h-8 w-auto"
+      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+      alt="Workflow"
+    />
+
+    <h2 class="hidden lg:block text-2xl font-bold leading-7 sm:truncate">
+      Safe Parker
+    </h2>
+  </div>
+</template>
---
+++ webapp/components/UserMenu.vue
@@ -0,0 +1,88 @@
+<template>
+  <div
+    class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+  >
+    <button
+      class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+    >
+      <span class="sr-only">View notifications</span>
+      <!-- Heroicon name: outline/bell -->
+      <svg
+        class="h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+        />
+      </svg>
+    </button>
+
+    <!-- Profile dropdown -->
+    <div class="ml-3 relative">
+      <div>
+        <button
+          id="user-menu"
+          type="button"
+          class="bg-white flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+          aria-expanded="false"
+          aria-haspopup="true"
+          @click="isUserMenuOpen = !isUserMenuOpen"
+        >
+          <span class="sr-only">Open user menu</span>
+          <img
+            class="h-8 w-8 rounded-full"
+            src="https://png.pngtree.com/png-vector/20190909/ourmid/pngtree-outline-user-icon-png-image_1727916.jpg"
+            alt=""
+          />
+        </button>
+      </div>
+
+      <!-- Dropdown menu -->
+      <div
+        v-if="isUserMenuOpen"
+        class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="user-menu"
+      >
+        <a
+          href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          role="menuitem"
+        >
+          Your Profile
+        </a>
+        <a
+          href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          role="menuitem"
+        >
+          Settings
+        </a>
+        <a
+          href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          role="menuitem"
+        >
+          Sign out
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isUserMenuOpen: false,
+    }
+  },
+}
+</script>
